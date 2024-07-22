ecef89d4d84b1981afdd1adf3220447acf1e7bb0
JaroslawX <ad.jaro@wp.pl>
2024-07-22 13:35:47 +0200

    Library context

---
+++ LibraryApi/Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Data;
+
+public class LibraryContext : DbContext
+{
+    public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+}
